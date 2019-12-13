71381dbdbad9317737d78ff6cf38922e73ab6562
sathak3 <sathak318@gmail.com>
2019-12-13 15:09:05 +0530

    second

--- controller/userapi.js
+++ controller/userapi.js
@@ -20,6 +20,11 @@ exports.create_a_user = function (req, res) {
     new_user.save(function (err, user) {
         if (err)
             res.send(err);
+        //res.json(user);
+    });
+    User.find({}, function (err, user) {
+        if (err)
+            res.send(err);
         res.json(user);
     });
 };
@@ -39,6 +44,11 @@ exports.update_a_user = function (req, res) {
     User.findOneAndUpdate({ userId: req.params.userId }, req.body, { new: true }, function (err, user) {
         if (err)
             res.send(err);
+       // res.json(user);
+    });
+     User.find({}, function (err, user) {
+        if (err)
+            res.send(err);
         res.json(user);
     });
 };
@@ -50,6 +60,11 @@ exports.delete_a_user = function (req, res) {
     }, function (err, User) {
         if (err)
             res.send(err);
-        res.json({ message: 'user successfully deleted' });
+       // res.json({ message: 'user successfully deleted' });
+    });
+    User.find({}, function (err, user) {
+        if (err)
+            res.send(err);
+        res.json(user);
     });
 };
