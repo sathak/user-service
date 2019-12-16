1f5a22f8985226d8c39869725a3dccb927cf4b64
smartdevelopers <sathak318@gmail.com>
2019-12-16 12:04:56 +0300

    update

--- index.js
+++ index.js
@@ -11,18 +11,17 @@
     mongoose.Promise = global.Promise;
     mongoose.connect(config["Mongo-URL"]);
     var routes = require('./helper/route');
-
-    
-    app.use(bodyParser.urlencoded({extended:true}));
-    app.use(bodyParser.json());
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
+    
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.use(bodyParser.json());
+
+
     
     routes(app);
     
