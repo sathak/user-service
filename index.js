095337dc214979d127d4bb5577869267427ba770
smartdevelopers <sathak318@gmail.com>
2019-12-17 17:42:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.all('*', function(req, res, next) {
      res.header('Access-Control-Allow-Origin', origin);
      res.header("Access-Control-Allow-Headers", "X-Requested-With");
      res.header('Access-Control-Allow-Headers', 'Content-Type');
+ res.header('Access-Control-Allow-Methods: POST,PUT,GET,DELETE');
      next();
 });
     
