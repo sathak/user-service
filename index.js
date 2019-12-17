f30977fc4bcabebf7ce1a4cca037bc5e3a5037b4
smartdevelopers <sathak318@gmail.com>
2019-12-17 17:53:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,13 @@
     mongoose.Promise = global.Promise;
     mongoose.connect(config["Mongo-URL"]);
     var routes = require('./helper/route');
-app.use(function (req, res, next) {
+ app.all('*', function(req, res, next) {
  var origin = req.get('origin'); 
      res.header('Access-Control-Allow-Origin', origin);
-     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Headers", "Accept");
+  res.header("Access-Control-Allow-Headers", "Authorization");
      res.header('Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
      next();
 });
