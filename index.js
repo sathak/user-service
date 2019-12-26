c3fa01e245195ffa140e9546c0d212790557f134
smartdevelopers <sathak318@gmail.com>
2019-12-26 11:32:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  var express = require('express'),
       app = express(),
       port = process.env.PORT || 3000,
+     expressSwagger = require('express-swagger-generator')(app),
       mongoose = require('mongoose'),
       User = require('./models/user'),
       bodyParser = require('body-parser');
@@ -24,6 +25,26 @@
 
     
     routes(app);
+
+let options = {
+    swaggerDefinition: {
+        info: {
+            description: 'This is a sample server',
+            title: 'Swagger',
+            version: '1.0.0',
+        },
+        host: 'localhost:3000',
+        basePath: '/v1',
+        produces: [
+            "application/json",
+            "application/xml"
+        ],
+        schemes: ['http', 'https']
+    },
+    basedir: __dirname, //app absolute path
+    files: ['./helper/route/*.js'] //Path to the API handle folder
+};
+expressSwagger(options)
     
 
     app.listen(port);
